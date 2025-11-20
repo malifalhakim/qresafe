62c866b88ef37e08f386bd3d0a2e88f7bb797a58
alif <malifalhakim11@gmail.com>
2025-11-20 17:19:21 +0700

    Add notification

--- quant-without-ft/temp.sh
+++ quant-without-ft/temp.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+send_notification(){
+    local message=$1
+    curl -d "$message" ntfy.sh/fair_safe_model_quant
+}
+
 source quant/bin/activate
 
 # --- QWEN QUANTIZATION ---
 ./pipeline.sh --model_path "Qwen/Qwen2.5-7B-Instruct" 
+send_notification "Qwen 2.5B Quantization Completed"
 
 # --- GEMMA QUANTIZATION Q-TRUST ---
 ./pipeline.sh --model_path "google/gemma-7b-it" --skip_baseline --skip_safety --skip_fairness
+send_notification "Gemma 7B Q-Trust Quantization Completed"
 
 # --- LLAMA QUANTIZATION Q-TRUST ---
 ./pipeline.sh --model_path "meta-llama/Llama-3.1-8B-Instruct" --skip_baseline --skip_safety --skip_fairness
+send_notification "Llama 3.1 8B Q-Trust Quantization Completed"
 
 deactivate
