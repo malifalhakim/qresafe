33be7b719906fddabc6ec80c6efbbc9edbbfcf62
alif <malifalhakim11@gmail.com>
2026-01-02 06:52:31 +0700

    Update temp.sh

--- quant-without-ft/temp.sh
+++ quant-without-ft/temp.sh
@@ -8,20 +8,20 @@ send_notification(){
 source quant/bin/activate
 
 # --- QWEN QUANTIZATION ---
-./pipeline.sh --model_path "Qwen/Qwen2.5-7B-Instruct" --skip_baseline --skip_fairness --skip_trust
-send_notification "Qwen 2.5B Quantization Completed"
+# ./pipeline.sh --model_path "Qwen/Qwen2.5-7B-Instruct" --skip_baseline --skip_fairness --skip_trust
+# send_notification "Qwen 2.5B Quantization Completed"
 
 # --- GEMMA QUANTIZATION Q-TRUST ---
-./pipeline.sh --model_path "google/gemma-7b-it" --skip_baseline --skip_fairness --skip_trust
-send_notification "Gemma 7B Q-Trust Quantization Completed"
+# ./pipeline.sh --model_path "google/gemma-7b-it" --skip_baseline --skip_fairness --skip_trust
+# send_notification "Gemma 7B Q-Trust Quantization Completed"
 
 # --- LLAMA QUANTIZATION Q-TRUST ---
-./pipeline.sh --model_path "meta-llama/Llama-3.1-8B-Instruct" --skip_baseline --skip_fairness --skip_trust
-send_notification "Llama 3.1 8B Q-Trust Quantization Completed"
+# ./pipeline.sh --model_path "meta-llama/Llama-3.1-8B-Instruct" --skip_baseline --skip_fairness --skip_trust
+# send_notification "Llama 3.1 8B Q-Trust Quantization Completed"
 
 # --- Tuning ---
-# ./tuning.sh --model_path "meta-llama/Llama-3.1-8B-Instruct"
-# send_notification "Llama 3.1 8B Tuning Completed"
+./tuning_tau.sh --model_path "meta-llama/Llama-3.1-8B-Instruct"
+send_notification "Llama 3.1 8B Tuning Completed"
 
 # --- DELETE /huggingface CACHE FOLDER ---
 rm -rf /huggingface/*
